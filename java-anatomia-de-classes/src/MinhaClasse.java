2ca5f3f16c0813e78f5dc4fa7377aa11d15e88f0
Vernan-Marques <vernanmarques2019@gmail.com>
2023-01-12 00:34:53 -0300

    Variable Declaration

--- java-anatomia-de-classes/src/MinhaClasse.java
+++ java-anatomia-de-classes/src/MinhaClasse.java
@@ -4,5 +4,27 @@ public static void main(String[] args) {
 
     System.out.print ( "Esse é o meu segundo projeto Java da Dio." );
 
+    String BR = " Brasil";
+
+    System.out.print(BR);
+
+    // ------Variable Declaration------
+
+    // Structure
+    // Tipo nomeBemDefinido = atribuição (opcional em alguns casos)
+
+    // Example
+
+    // String
+    String myName = "Vernan";
+
+    // Integer
+    int anoFabricacao = 2023;
+
+    // Logical Value
+    boolean verdadeira = false;
+
+    anoFabricacao = 2019; // Changed the value of the variable
+
 }
 }
