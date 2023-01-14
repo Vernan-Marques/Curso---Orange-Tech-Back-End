feeedbdd4dd7395327a9da642a7000b8fcb36527
Vernan-Marques <vernanmarques2019@gmail.com>
2023-01-13 21:50:32 -0300

    code indentation

---
+++ Métodos/src/StudentReport.java
@@ -0,0 +1,14 @@
+public class StudentReport {
+    public static void main (String[] args) {
+
+        int finalMedia = 8;
+
+        if(finalMedia < 6)
+            System.out.println("\nREPROVADO! :-(");
+        else if (finalMedia == 6)
+            System.out.println("\nVOCÊ ESTÁ DE RECUPERAÇÃO :-|");
+        else
+            System.out.println("\nAprovado! :-)");
+
+    }
+}
