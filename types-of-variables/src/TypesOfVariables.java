aff046236a527585fe11cf06eec6aa760909066b
Vernan-Marques <vernanmarques2019@gmail.com>
2023-01-16 12:07:19 -0300

    Variables and data types primitive

--- types-of-variables/src/TypesOfVariables.java
+++ types-of-variables/src/TypesOfVariables.java
@@ -1,5 +1,23 @@
 public class TypesOfVariables {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int age;
+
+        int yearManufacture = 2023;
+
+        double minimumWage = 2500.33;
+
+        int cep = 48495623;
+        
+        long cpf = 54168448954L;
+
+        float pi = 3.14F;
+
+        double wage = 1300.65;
+
+        // Cast
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
     }
 }
