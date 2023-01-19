4612e9e647863fa92d829b4a4ceb81c478818b20
Vernan-Marques <vernanmarques2019@gmail.com>
2023-01-19 10:18:16 -0300

    Increment and Negation

---
+++ operators/src/Increment.java
@@ -0,0 +1,16 @@
+public class Increment {
+    public static void main(String[] args) {
+
+        // Increment
+        int number = 3;
+
+        number++; // number = number + 1;
+
+        System.out.println(number);
+
+        // Boolean
+        boolean variable = true;
+
+        System.out.println(!variable); // (!) - negation operator
+    }
+}
