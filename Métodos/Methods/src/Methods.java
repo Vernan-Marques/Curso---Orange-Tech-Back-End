b79358d540f488083cffb25b9a56d4709ec79d91
Vernan-Marques <vernanmarques2019@gmail.com>
2023-01-23 15:41:09 -0300

    Exemples 1

---
+++ Métodos/Methods/src/Methods.java
@@ -0,0 +1,13 @@
+public class Methods {
+    public static void main(String[] args) throws Exception {
+
+        //Exemples of Methods
+        sum (int n1, int n2) {}
+
+        openConnection () {}
+
+        finishProcessing () {}
+
+        findById(int id){}
+    }
+}
