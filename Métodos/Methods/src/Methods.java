0ddb1a8d8fbaeb5fb7820a09e31da76e87219798
Vernan-Marques <vernanmarques2019@gmail.com>
2023-01-24 23:14:20 -0300

    Visibility and Exceptions

--- Métodos/Methods/src/Methods.java
+++ Métodos/Methods/src/Methods.java
@@ -10,4 +10,31 @@ public class Methods {
 
         findById(int id){}
     }
+
+    // method visibility_______
+
+    public double add(int numb1, int numb2) {
+        
+        return ... ;// Will return something
+    }
+
+    public void imprimir(String texto) {
+        // Void - Will not return anything
+    }
+
+    // throws Exception : Indicated that the method may generate an exception
+    public double dividir(int dividendo, int divisor) throws Exception{}
+
+    // private - This method cannot be seen by other classes
+    private void privateMethod(){}
+
+    public void gravarCliente(String nome, String cpf){
+        // This method is intended to record customer information. 
+        // Why not create a client object and pass it as a parameter?
+        // See below:
+    }
+
+    public void gravarCliente(Cliente cliente){}
+    // or
+    public void gravar(Cliente cliente) {}
 }
