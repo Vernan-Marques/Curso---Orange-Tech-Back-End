4e0e382c31c9f5cad282055ec8bd59c4b926dd6a
Vernan-Marques <vernanmarques2019@gmail.com>
2023-02-25 08:27:42 -0300

    Commit on NameAge

--- Exercises/src/loops/NameAndAge.java
+++ Exercises/src/loops/NameAndAge.java
@@ -1,5 +1,11 @@
 package loops;
 
+/*
+ Write a program that reads sets of two values. 
+ The first representing the student's name and the second representing his age. 
+ (Stop the program by entering the value 0 in the name field.)
+ */
+
 import java.util.Scanner;
 
 public class NameAndAge {
