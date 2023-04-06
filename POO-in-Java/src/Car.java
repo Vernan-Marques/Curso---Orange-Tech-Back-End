1ea1e20e0b72336c836114b654541f8d9a4d7c62
Vernan Marques <vernanmarques2019@gmail.com>
2023-04-05 23:30:06 -0300

    Methods Demonstration

--- POO-in-Java/src/Car.java
+++ POO-in-Java/src/Car.java
@@ -1,5 +1,47 @@
 public class Car {
-    String cor;
-    String modelo;
-    int capacidadeTanque;
+    String color;
+    String model;
+    int tankCapacity;
+
+    // Constructor here______________
+
+    // Overload
+    Car (){
+
+    }
+    Car(String color, String model, int tankCapacity){
+        this.color = color;
+        this.model = model;
+        this.tankCapacity = tankCapacity;
+    }
+
+    //____________________________
+
+    void setColor(String color){
+        this.color = color;
+    }
+
+    String getColor(String color){
+        return color;
+    }
+
+    void setModel(String model) {
+        this.model = model;
+    }
+
+    String getModel(){
+        return model;
+    }
+
+    void setTankCapacity(int tankCapacity){
+        this.tankCapacity = tankCapacity;
+    }
+
+    int getTankCapacity(){
+        return tankCapacity;
+    }
+
+    double totalTankValue(double fuelValue){
+        return tankCapacity * fuelValue;
+    }
 }
